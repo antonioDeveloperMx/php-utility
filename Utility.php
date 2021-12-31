fa59ff23678b554409c9660a8da5fadddb2b1419
antonioDeveloperMx <antoniolopez.developer@gmail.com>
2021-12-31 08:39:07 -0600

    Add main class

---
+++ Utility.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antonio
+ * Date: 15/10/2019
+ * Time: 01:03 PM
+ */
+
+namespace App\Utilities;
+
+class Utility
+{
+    public static function EncryptPassword($seed, $password){
+        return sha1(md5($seed.strip_tags(trim($password))));
+    }
+
+    public static function QuickRandomStr($length = 16){
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+    }
+
+    public static function QuickRandomNumber($length = 16){
+        $pool = '0123456789';
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+    }
+
+    public static function DistanceCalculation($point1_lat, $point1_long, $point2_lat,
+        $point2_long, $unit = 'km', $decimals = 2){
+
+	    @$degrees = rad2deg(acos((sin(deg2rad($point1_lat))*sin(deg2rad($point2_lat))) + (cos(deg2rad($point1_lat))*cos(deg2rad($point2_lat))*cos(deg2rad($point1_long-$point2_long)))));
+        switch($unit) {
+            case 'km':
+                $distance = $degrees * 111.13384; // 1 grado = 111.13384 km, basándose en el diametro promedio de la Tierra (12.735 km)
+                break;
+            case 'mi':
+                $distance = $degrees * 69.05482; // 1 grado = 69.05482 millas, basándose en el diametro promedio de la Tierra (7.913,1 millas)
+                break;
+            case 'nmi':
+                $distance =  $degrees * 59.97662; // 1 grado = 59.97662 millas naúticas, basándose en el diametro promedio de la Tierra (6,876.3 millas naúticas)
+        }
+        return round($distance, $decimals);
+    }
+
+    public static function GetDeliveryTime($point1_lat, $point1_long, $point2_lat,
+        $point2_long, $unit = 'km', $decimals = 2){
+
+        $km = Utility::DistanceCalculation($point1_lat, 
+        $point1_long, $point2_lat, $point2_long, 'km', $decimals);
+
+        $time = Math.round(($km * 22) / 5.4);
+		if($time < 15){
+			$time = 15;
+		}
+		$time = $time + 10;
+
+    }
+
+    public static function FixWrongUTF8Encoding($inputString) {
+        $fix_list = array(
+            // 3 char errors first
+            'â€š' => '‚', 'â€ž' => '„', 'â€¦' => '…', 'â€¡' => '‡',
+            'â€°' => '‰', 'â€¹' => '‹', 'â€˜' => '‘', 'â€™' => '’',
+            'â€œ' => '“', 'â€¢' => '•', 'â€“' => '–', 'â€”' => '—',
+            'â„¢' => '™', 'â€º' => '›', 'â‚¬' => '€',
+            // 2 char errors
+            'Ã‚'  => 'Â', 'Æ’'  => 'ƒ', 'Ãƒ'  => 'Ã', 'Ã„'  => 'Ä',
+            'Ã…'  => 'Å', 'â€'  => '†', 'Ã†'  => 'Æ', 'Ã‡'  => 'Ç',
+            'Ë†'  => 'ˆ', 'Ãˆ'  => 'È', 'Ã‰'  => 'É', 'ÃŠ'  => 'Ê',
+            'Ã‹'  => 'Ë', 'Å’'  => 'Œ', 'ÃŒ'  => 'Ì', 'Å½'  => 'Ž',
+            'ÃŽ'  => 'Î', 'Ã‘'  => 'Ñ', 'Ã’'  => 'Ò', 'Ã“'  => 'Ó',
+            'â€'  => '”', 'Ã”'  => 'Ô', 'Ã•'  => 'Õ', 'Ã–'  => 'Ö',
+            'Ã—'  => '×', 'Ëœ'  => '˜', 'Ã˜'  => 'Ø', 'Ã™'  => 'Ù',
+            'Å¡'  => 'š', 'Ãš'  => 'Ú', 'Ã›'  => 'Û', 'Å“'  => 'œ',
+            'Ãœ'  => 'Ü', 'Å¾'  => 'ž', 'Ãž'  => 'Þ', 'Å¸'  => 'Ÿ',
+            'ÃŸ'  => 'ß', 'Â¡'  => '¡', 'Ã¡'  => 'á', 'Â¢'  => '¢',
+            'Ã¢'  => 'â', 'Â£'  => '£', 'Ã£'  => 'ã', 'Â¤'  => '¤',
+            'Ã¤'  => 'ä', 'Â¥'  => '¥', 'Ã¥'  => 'å', 'Â¦'  => '¦',
+            'Ã¦'  => 'æ', 'Â§'  => '§', 'Ã§'  => 'ç', 'Â¨'  => '¨',
+            'Ã¨'  => 'è', 'Â©'  => '©', 'Ã©'  => 'é', 'Âª'  => 'ª',
+            'Ãª'  => 'ê', 'Â«'  => '«', 'Ã«'  => 'ë', 'Â¬'  => '¬',
+            'Ã¬'  => 'ì', 'Â®'  => '®', 'Ã®'  => 'î', 'Â¯'  => '¯',
+            'Ã¯'  => 'ï', 'Â°'  => '°', 'Ã°'  => 'ð', 'Â±'  => '±',
+            'Ã±'  => 'ñ', 'Â²'  => '²', 'Ã²'  => 'ò', 'Â³'  => '³',
+            'Ã³'  => 'ó', 'Â´'  => '´', 'Ã´'  => 'ô', 'Âµ'  => 'µ',
+            'Ãµ'  => 'õ', 'Â¶'  => '¶', 'Ã¶'  => 'ö', 'Â·'  => '·',
+            'Ã·'  => '÷', 'Â¸'  => '¸', 'Ã¸'  => 'ø', 'Â¹'  => '¹',
+            'Ã¹'  => 'ù', 'Âº'  => 'º', 'Ãº'  => 'ú', 'Â»'  => '»',
+            'Ã»'  => 'û', 'Â¼'  => '¼', 'Ã¼'  => 'ü', 'Â½'  => '½',
+            'Ã½'  => 'ý', 'Â¾'  => '¾', 'Ã¾'  => 'þ', 'Â¿'  => '¿',
+            'Ã¿'  => 'ÿ', 'Ã€'  => 'À',
+            // 1 char errors last
+            'Ã' => 'Á', 'Å' => 'Š', 'Ã' => 'Í', 'Ã' => 'Ï',
+            'Ã' => 'Ð', 'Ã' => 'Ý', 'Ã' => 'á', 'Ã­' => 'í',
+            'Â' => '',
+        );
+
+        $error_chars = array_keys($fix_list);
+        $real_chars  = array_values($fix_list);     
+
+        return trim(str_replace($error_chars, $real_chars, $inputString));
+
+    }
+
+    public static function eliminar_acentos($cadena)
+    {
+        //Reemplazamos la A y a
+        $cadena = str_replace(
+            array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+            array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+            $cadena
+        );
+
+        //Reemplazamos la E y e
+        $cadena = str_replace(
+            array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+            array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+            $cadena
+        );
+
+        //Reemplazamos la I y i
+        $cadena = str_replace(
+            array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+            array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+            $cadena
+        );
+
+        //Reemplazamos la O y o
+        $cadena = str_replace(
+            array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+            array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+            $cadena
+        );
+
+        //Reemplazamos la U y u
+        $cadena = str_replace(
+            array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+            array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+            $cadena
+        );
+
+        //Reemplazamos la N, n, C y c
+        $cadena = str_replace(
+            array('Ñ', 'ñ', 'Ç', 'ç'),
+            array('N', 'n', 'C', 'c'),
+            $cadena
+        );
+
+        return $cadena;
+    }
+
+    public static function FirstUpperCase($text) {
+        return mb_convert_case($text, MB_CASE_TITLE, "UTF-8");
+    }
+    public static function contains_space($texto, $palabra){
+        return preg_match('*\b' . preg_quote($palabra) . '\b*i', $texto);
+    }
+
+    public static function GetSingular($phrase){
+        if(!strpos($phrase, " ")){
+            if (Utility::EndsWith($phrase, 'es')) 
+                $phrase = mb_substr($phrase, 0, -2);   
+            else if(Utility::EndsWith($phrase, 's'))
+                $phrase = mb_substr($phrase, 0, -1);
+        }
+        return $phrase;
+    }
+
+    public static function StartsWith( $haystack, $needle ) {
+        $length = strlen( $needle );
+        return substr( $haystack, 0, $length ) === $needle;
+    }
+   
+    public static function EndsWith( $haystack, $needle ) {
+        $length = strlen( $needle );
+        if( !$length ) {
+            return true;
+        }
+        return substr( $haystack, -$length ) === $needle;
+    }
+   
+
+}
